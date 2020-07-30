b91312754b59026d5756e4642f1a4161440a45fb
Lado Samushia <lado.samushia.office@gmail.com>
2020-07-29 21:32:07 -0500

    add runtests

---
+++ test/runtests.jl
@@ -0,0 +1,9 @@
+using Test, SafeTestsets
+
+begin
+    @safetestset "Utilities" begin include("utilities_test.jl") end
+    @safetestset "Grid" begin include("grid_test.jl") end
+    @safetestset "Bispectrum utilities" begin include("bispectrum_utilities_test.jl") end
+    @safetestset "Power Spectrum" begin include("powerspectrum_test.jl") end
+    @safetestset "Bispectrum" begin include("bispectrum_test.jl") end
+end
