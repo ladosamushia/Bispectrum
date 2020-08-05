a86a857eec5eb048fdb9dc22b41a88c5cbb5453d
Samushia <lado@phys.ksu.edu>
2020-08-05 13:18:47 -0500

    Minor

--- exact_but_slow.jl
+++ exact_but_slow.jl
@@ -24,13 +24,13 @@ function bispectrum_exact(grid_k, dk, L, kmax)
 
     for i1 in 1:Nmax
         @threads for i in 1:Nmax
-            loop_over_k1k2_exact(kx, ky, kz, Nmax, Ngrid, i, threadid(), grid_k)
+            loop_over_k1k2_exact(kx, ky, kz, Nmax, Ngrid, i, threadid(), grid_k, Nk, Bk)
         end
     end
     return sum(Bk, dims=1) ./ sum(Nk, dims=1) * (L / Nz)^6 / Nz^3
 end
 
-function loop_over_k1k2_exact(kx, ky, kz, Nmax, Ngrid, i1, tid, grid_k)
+function loop_over_k1k2_exact(kx, ky, kz, Nmax, Ngrid, i1, tid, grid_k, Nk, Bk)
     kx1 = kx[i1]
     for j1 in flatten((1:Nmax, Ngrid - Nmax + 1:Ngrid))
         ky1 = ky[j1]
