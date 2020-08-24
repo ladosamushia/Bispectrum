da5b70a47485da1e341ad95b88cc31831042e31c
Lado Samushia <lado.samushia.office@gmail.com>
2020-08-24 00:56:28 -0500

    some docs

--- scripts/compute_bispectrum.jl
+++ scripts/compute_bispectrum.jl
@@ -2,6 +2,18 @@ include("../src/powerspectrum.jl")
 include("../src/grid.jl")
 include("../src/bispectrum.jl")
 
+"""
+    compute_pk_bk(x, y, z, dk, N, L, outfile)
+
+    Compute Bk and Pk given x, y, z, coordinates.
+
+    # Parameters
+    - `x, y, z::{Array, Float}`: Cartesian coordinates.
+    - `dk::Float`: Bin size.
+    - `N::Int`: Number of bins.
+    - `L::Float`: Size of the cube.
+    - `outfile::string`: Output text file. Will be proceeded by "bk_", "pk_" respectively.
+"""
 function compute_pk_bk(x, y, z, dk, N, L, outfile)
     gr = grid_r(512, x, y, z)
     gk = grid_k(gr)
