17a5164eb3dfada0bf9c774e6e01214177697e16
Lado Samushia <lado.samushia.office@gmail.com>
2023-02-14 08:10:59 -0600

    all tests have passed

--- test/grid_test.jl
+++ test/grid_test.jl
@@ -8,36 +8,32 @@ y = [0, 4.5, 10]
 z = [0, 4.5, 10]
 gr = grid_r(10, x, y, z, 4)
 
-for i in -1:1
-    for j in -1:1
-        for k in -1:1
-            sx = abs(i)
-            if sx > 2
-                w = 0
-            elseif sx < 1
-                w = (4 - 6*sx^2 + 3*sx^3)/6
-            else
-                w = (2 - sx)^3/6
-            end
-            sy = abs(j)
-            if sy > 2
-                w *= 0
-            elseif sy < 1
-                w *= (4 - 6*sy^2 + 3*sy^3)/6
-            else
-                w *= (2 - sy)^3/6
-            end   
-            sz = abs(k)
-            if sz > 2
-                w *= 0
-            elseif sz < 1
-                w *= (4 - 6*sz^2 + 3*sz^3)/6
-            else
-                w *= (2 - sz)^3/6
-            end                      
-            @test isapprox(gr[5 + i, 5 + j, 5 + k], w)
-        end
+for i in -1:1, j in -1:1, k in -1:1
+    sx = abs(i)
+    if sx > 2
+        w = 0
+    elseif sx < 1
+        w = (4 - 6*sx^2 + 3*sx^3)/6
+    else
+        w = (2 - sx)^3/6
     end
+    sy = abs(j)
+    if sy > 2
+        w *= 0
+    elseif sy < 1
+        w *= (4 - 6*sy^2 + 3*sy^3)/6
+    else
+        w *= (2 - sy)^3/6
+    end   
+    sz = abs(k)
+    if sz > 2
+        w *= 0
+    elseif sz < 1
+        w *= (4 - 6*sz^2 + 3*sz^3)/6
+    else
+        w *= (2 - sz)^3/6
+    end                      
+    @test isapprox(gr[5 + i, 5 + j, 5 + k], w/size(x)[1]*size(gr)[1]^3 - 1)
 end
 
 gr = zeros(10,10,10)
@@ -53,17 +49,17 @@ for i in 2:6
         end
     end
 end
-@test gk[1, 1, 1] == 1000
+@test isapprox(Real(gk[1, 1, 1]), length(gr); atol=1e-6)
 
 Npart = 100000
 x = rand(Npart)*1000
 y = rand(Npart)*1000
 z = rand(Npart)*1000
 gr = grid_r(512, x, y, z, 1)
-@test isapprox(sum(gr), Npart)
+@test isapprox(sum(gr), 0; atol=1e-6)
 gr = grid_r(512, x, y, z, 2)
-@test isapprox(sum(gr), Npart)
+@test isapprox(sum(gr), 0; atol=1e-6)
 gr = grid_r(512, x, y, z, 3)
-@test isapprox(sum(gr), Npart)
+@test isapprox(sum(gr), 0; atol=1e-6)
 gr = grid_r(512, x, y, z, 4)
-@test isapprox(sum(gr), Npart)
+@test isapprox(sum(gr), 0; atol=1e-6)
